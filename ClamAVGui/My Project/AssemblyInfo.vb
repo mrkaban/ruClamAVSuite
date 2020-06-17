234ebf89b572424aeb64baf2d48ebdc58373eed3
Алексей <s-r-grass@mail.ru>
2020-06-17 19:24:05 +0700

    Add files via upload

---
+++ ClamAVGui/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Le informazioni generali relative a un assembly sono controllate dal seguente 
+' set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+' associate a un assembly.
+
+' Rivedere i valori degli attributi degli assembly
+
+<Assembly: AssemblyTitle("ClamAVGui")>
+<Assembly: AssemblyDescription("A security suite for Windows 10.")>
+<Assembly: AssemblyCompany("A.Mazzeo")>
+<Assembly: AssemblyProduct("ClamAVSuite")>
+<Assembly: AssemblyCopyright("Copyright ©  2016")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(True)>
+
+'Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+<Assembly: Guid("138a6261-f2ec-44ac-a638-96dbb3088b23")>
+
+' Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+'
+'      Versione principale
+'      Versione secondaria 
+'      Numero di build
+'      Revisione
+'
+' È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
+' usando l'asterisco '*' come illustrato di seguito:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
